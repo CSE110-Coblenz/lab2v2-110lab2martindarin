395a58b8fe3cc99192c94156f0982cb878810a70
martincorona <m4corona@ucsd.edu>
2025-10-09 14:52:33 -0700

    animate change

--- src/music.ts
+++ src/music.ts
@@ -1,11 +1,11 @@
 import { animateFeature } from './animation';
 
-const music = ["Hip-Hop", "Rap", "Pop", "R&B"];
+const music = ["Hip-Hop", "Rap", "Pop", "R&B", "Jazz"];
 
 export function printMusic() {
   console.log("Music for the party:");
   music.forEach((genre) => console.log("- " + genre));
 }
 
-
+animateFeature("Music");
 printMusic();
