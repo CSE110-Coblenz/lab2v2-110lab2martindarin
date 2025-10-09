2236c27e841279cf8a58ac4f259d7bc2ecd91f65
martincorona <m4corona@ucsd.edu>
2025-10-09 15:07:20 -0700

    fixed a double printing bug

--- src/music.ts
+++ src/music.ts
@@ -3,9 +3,7 @@ import { animateFeature } from './animation';
 const music = ["Hip-Hop", "Rap", "Pop", "R&B", "Jazz"];
 
 export function printMusic() {
+  animateFeature("Music");
   console.log("Music for the party:");
   music.forEach((genre) => console.log("- " + genre));
 }
-
-animateFeature("Music");
-printMusic();
